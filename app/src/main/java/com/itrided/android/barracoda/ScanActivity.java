fbfe720f53fe7b4f8283c18d7b35e8e8891613c2
Daniel Milkov <daniel.milkov@wacom.eu>
2018-07-21 23:08:11 +0300

    Added barcode scanning demo
    Added INTERNET permission to manifest;
    Added layout tags on all existing layouts for data binding;
    Added play services and picasso libraries;
    Added new app theme colors;
    Reworked ScanActivity to make use of the data binding library;

--- app/src/main/java/com/itrided/android/barracoda/ScanActivity.java
+++ app/src/main/java/com/itrided/android/barracoda/ScanActivity.java
@@ -1,25 +1,93 @@
 package com.itrided.android.barracoda;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.SparseArray;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.itrided.android.barracoda.databinding.ActivityScanBinding;
+import com.itrided.android.barracoda.databinding.AppBarScanBinding;
+import com.itrided.android.barracoda.databinding.ContentScanBinding;
+import com.squareup.picasso.Picasso;
 
 public class ScanActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-//region API Methods
+    private ActivityScanBinding activityScanBinding;
+    private AppBarScanBinding appBarScanBinding;
+    private ContentScanBinding contentScanBinding;
+    private BarcodeDetector barcodeDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityScanBinding = DataBindingUtil.setContentView(this, R.layout.activity_scan);
+        appBarScanBinding = activityScanBinding.appBarScan;
+        contentScanBinding = appBarScanBinding.contentScan;
+
+        activityScanBinding.navView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(appBarScanBinding.toolbar);
+        setupDrawerToggle();
+        setupBarcodeDetector();
+
+        appBarScanBinding.fab.setOnClickListener(view ->
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show());
+
+        contentScanBinding.processBtn.setOnClickListener(v -> runBarcodeDemo());
+    }
+
+    private void setupBarcodeDetector() {
+        barcodeDetector = new BarcodeDetector.Builder(getApplicationContext())
+                .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                .build();
+        if (!barcodeDetector.isOperational()) {
+            contentScanBinding.contentTv.setText(R.string.barcode_detector_error);
+        }
+    }
+
+    private void runBarcodeDemo() {
+        final Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+
+        final Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        final SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
+        final Barcode barcode = barcodes.valueAt(0);
+
+        contentScanBinding.contentTv.setText(barcode.rawValue);
+        final String url = barcode.rawValue.replaceAll("^.*\\s", "");
+
+        Picasso.get()
+                .load(url)
+                .into(contentScanBinding.barcodeIv);
+    }
+
+    private void setupDrawerToggle() {
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, activityScanBinding.drawerLayout, appBarScanBinding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        activityScanBinding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = activityScanBinding.drawerLayout;
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -27,31 +95,9 @@ public class ScanActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.scan, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -69,37 +115,7 @@ public class ScanActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        activityScanBinding.drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-//region API Methods
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-//endregion API Methods
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-//endregion API Methods
 }
